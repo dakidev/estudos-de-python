8d8d5b469c3946c9f6e170e7c7d8c4525b7b3297
Daki <64710575+dakidev@users.noreply.github.com>
2020-09-22 16:45:53 -0300

    Update desafio3.py

--- CursoEmVideo/Python/desafio3.py
+++ CursoEmVideo/Python/desafio3.py
@@ -9,4 +9,5 @@ n2 = int(input('Digite outro número: '))
 soma = n1+n2
 
 #saida
-print('A soma entre', n1, 'e', n2, 'é igual a', soma)
+#print('A soma entre', n1, 'e', n2, 'é igual a', soma)
+print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
